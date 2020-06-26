944d79359d8fe7a720bbc584376819d83a64053f
leegiseo <65993920+leegiseo@users.noreply.github.com>
2020-06-26 12:35:21 +0900

    #(N+2)
    #(N+2)

---
+++ calc/template.py
@@ -0,0 +1,13 @@
+html="""
+<html>
+    <body>
+        <form action="">
+            first number = <inpyt type = "number" name = "first_num"><br><br>
+            second number = <inpyt type = "number" name = "second_num">
+            <input type="submit">
+        </form>
+        sum=%(sum)s <br><br>
+        Mul=%(mul)s
+    </body>
+</html>
+"""
